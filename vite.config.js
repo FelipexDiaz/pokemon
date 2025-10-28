139ca80e1839de0663a4bc77fd8d690555488a67
Felipe Diaz <FELIPEXDIAZ@HOTMAIL.COM>
2025-10-28 20:21:01 -0300

    docs

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/pokemon/', // <- NOMBRE EXACTO DE TU REPO
+  base: '/pokemon/',       // tu repo
+  build: {
+    outDir: 'docs',         // Vite genera el build en /docs en lugar de /dist
+  },
 })
